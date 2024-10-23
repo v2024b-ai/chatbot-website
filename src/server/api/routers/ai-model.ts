f9a90699e014475366c382f9dcdb6d02ea76fcea
jdwalden74 <jdwalden2@gmail.com>
2024-10-23 11:06:57 +0200

    made intiL schema and a add model route

---
+++ src/server/api/routers/ai-model.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const addModelInput = z.object({
+  model: z.string(),
+  url: z.string(),
+  ppInput: z.number().nullable(),
+  ppOutput: z.number().nullable(),
+  termsURL: z.string().nullable(),
+  ethicalConcern: z.boolean().nullable(),
+  ctxLength: z.number().nullable(),
+  modelSize: z.number().nullable(),
+  perplexity: z.string().nullable(),
+  bleu: z.number().nullable(),
+  rouge: z.number().nullable(),
+  meteor: z.number().nullable(),
+  inputResponseTime: z.number().nullable(),
+  outputResponseTime: z.number().nullable(),
+  maxOutput: z.number().nullable(),
+  maxInput: z.number().nullable(),
+  fileInput: z.boolean().nullable(),
+  fileOutput: z.boolean().nullable(),
+  features: z.array(z.string()),
+});
+
+export const modelRoute = createTRPCRouter({
+  addModel: publicProcedure
+    .input(addModelInput)
+    .mutation(async ({ input, ctx }) => {
+      return ctx.db.aiEval.create({
+        data: { ...input },
+      });
+    }),
+});
