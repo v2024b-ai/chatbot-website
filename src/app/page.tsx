512f1e203491f798054698b7543b23724002c6c4
jlamenha <lamenhajoaolucas@gmail.com>
2024-10-24 13:52:10 +0200

    fix color issue in front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-primary bg-gradient-to-b from-primary to-secondary text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-black">
+          <h1 className="text-5xl font-extrabold tracking-tight text-black">
             Welcome to the{" "}
             <span className="text-white drop-shadow-md">ChatVPC</span> App
           </h1>
