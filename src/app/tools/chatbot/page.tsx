28d7cebe2ae68b521a195eec900517f553fe420f
jlamenha <lamenhajoaolucas@gmail.com>
2024-10-24 10:51:49 +0200

    %apos; fix

--- src/app/tools/chatbot/page.tsx
+++ src/app/tools/chatbot/page.tsx
@@ -4,7 +4,7 @@ export default async function ChatbotPage(){
     return (
         <HydrateClient>
             <main className='flex min-h-screen flex-col items-center justify-center bg-primary bg-gradient-to-b from-primary to-secondary text-white'>
-                <p> Non c'è ancora niente!</p>
+                <p> Non c&apos;è ancora niente!</p>
             </main>
         </HydrateClient>
         
