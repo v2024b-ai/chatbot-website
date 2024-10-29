5e8f2f4db6ad848a1c9f5822eaec5157ee6dcaa8
jlamenha <lamenhajoaolucas@gmail.com>
2024-10-29 11:21:22 +0100

    updated reference and promp texts

--- python-backend/app.py
+++ python-backend/app.py
@@ -28,7 +28,11 @@ async def get_data():
 
 @app.post('/api/test-scoring')
 async def post_data(input_data: InputData):
-    reference_summary = "The cat sat on the mat."
+    reference_summary = {"1,000 people mourned Matthew Shepherd, the gay University of Wyoming student who was severely beaten and left to die tied to a fence." + 
+                        "The crime sparked nationwide vigils and prompted President Clinton to call for federal hate-crimes legislation." + 
+                        "In 19 states, including Colorado, sexual orientation is not included in hate-crime laws." + 
+                        "Wyoming is one of 10 states with no hate-crime laws at all, but the governor appealed to lawmakers to reconsider their opposition." + 
+                        "Christian conservatives argue that hate-crime laws restrict freedom of speech, while gay rights activists and others say these laws send a message that attacks on minorities will not be tolerated."} 
     
     scores = grade_summary(reference_summary, input_data.genStr)
     return JSONResponse(content=scores)
