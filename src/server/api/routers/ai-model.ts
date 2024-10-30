352e4e16995a4660f8bb644c16f8baf0ba44450b
jlamenha <lamenhajoaolucas@gmail.com>
2024-10-30 11:00:52 +0100

    changes in progress

--- src/server/api/routers/ai-model.ts
+++ src/server/api/routers/ai-model.ts
@@ -1,11 +1,26 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { addModelSchema } from "@/types/ai/models/model-eval-info-types";
+import axios from 'axios';
+
+
+interface PostData {
+  genStr: string;
+}
 
 export const modelRoute = createTRPCRouter({
   addModel: publicProcedure
     .input(addModelSchema)
     .mutation(async ({ input, ctx }) => {
+
+      const submitEval = async () => {
+        
+        try{
+          const Eval = await postEval({genStr: input.modelOutput});
+          console.log("Eval result: ", Eval)
+        }
+      }
+
       return ctx.db.aiEval.create({
         data: { ...input },
       });
@@ -21,3 +36,20 @@ export const modelRoute = createTRPCRouter({
     return ctx.db.aiEval.findMany();
   }),
 });
+
+const postEval = async (data: ): Promise<any> => {
+
+  const url = "chatvpc-python.vercel.app/test-scoring/"
+
+  try {
+    const evals = await axios.post(url, data, {
+      headers: {
+        'Content Type': 'application/json',
+      },
+    });
+    return evals.data;
+  } catch (error) {
+    console.error('Error posting evals:', error);
+    throw error;
+  }
+}
