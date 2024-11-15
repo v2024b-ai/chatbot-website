139ddc48320847ab990c992a5777d1d0d458e082
jlamenha <lamenhajoaolucas@gmail.com>
2024-11-15 12:47:48 +0100

    fixed descrip use

--- src/data/teams/B24pioneer/B24-pioneerTeam.ts
+++ src/data/teams/B24pioneer/B24-pioneerTeam.ts
@@ -1,42 +1,36 @@
-import type { TeamCards,MemberCards } from "@/types/team-cards";
+import type { TeamCards, MemberCards } from "@/types/team-cards";
 import Lucas from "@/data/teams/B24pioneer/LucasLamenha.jpeg";
 import Mina from "@/data/teams/B24pioneer/MinaBoktor.jpeg";
 import James from "@/data/teams/B24pioneer/JamesWalden.jpeg";
 import Krishna from "@/data/teams/B24pioneer/KrishnaGarg.jpeg";
 
 export const B24Members: Array<MemberCards> = [
-    {
-        name: "Mina Boktor",
-        major: "Computer Science '26",
-        picture: Mina,
-        descrip: ""
-    },
-    {
-        name: "Krishna Garg",
-        major: "Computer Science '26",
-        picture: Krishna,
-        descrip: ""
-    },
-    {
-        name: "Lucas Lamenha",
-        major: "Computer Science '25",
-        picture: Lucas,
-        descrip: "A senior Computer Science major at Worcester Polytechnic Institute from Recife, Brazil. Passionate about learning new languages, playing musical instruments and complex coding problems."
-    },
-    {
-        name: "James Walden",
-        major: "Computer Science '26",
-        picture: James,
-        descrip: ""
-    }
-]
-
-
-
+  {
+    name: "Mina Boktor",
+    major: "Computer Science '26",
+    picture: Mina,
+  },
+  {
+    name: "Krishna Garg",
+    major: "Computer Science '26",
+    picture: Krishna,
+  },
+  {
+    name: "Lucas Lamenha",
+    major: "Computer Science '25",
+    picture: Lucas,
+  },
+  {
+    name: "James Walden",
+    major: "Computer Science '26",
+    picture: James,
+  },
+];
 
 export const B24Pioneers: TeamCards = {
-    title: "ChatVPC",
-    term: "B2024",
-    description: "This was the first team to work on the ChatVPC project! They were the best.",
-    member: B24Members
-}
+  title: "ChatVPC",
+  term: "B2024",
+  description:
+    "This was the first team to work on the ChatVPC project! They were the best.",
+  member: B24Members,
+};
