47253e6a6edaddd3573448978eaade9261cac40e
minaboktor2628 <mina26b8@gmail.com>
2024-11-19 10:49:15 +0100

    init

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,10 @@
+import {
+  type GenerativeModel,
+  GoogleGenerativeAI,
+  TaskType,
+} from "@google/generative-ai";
 import "server-only";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 import axios from "axios";
 import fs from "fs";
@@ -11,11 +16,114 @@ import {
 } from "@/types/ai/gemini";
 import { capitalizeFirstLetter } from "./utils";
 import { type Readable } from "stream";
+import { db } from "@/server/db";
+import pdf from 'pdf-parse'
+
+export class SemanticRetriever {
+  private AIEmbeddingName = { model: "text-embedding-004" };
+  private GenAI: GoogleGenerativeAI
+  private Model: GenerativeModel
+
+  constructor() {
+    this.GenAI = new GoogleGenerativeAI(
+      process.env.GEMINI_API_KEY!
+    );
+
+    this.Model = this.GenAI.getGenerativeModel(
+      this.AIEmbeddingName,
+    );
+
+    console.log("init")
+  }
+
+  // takes in a string and returns embedding
+  async embed(data: string) {
+    const result = await this.Model.embedContent(data)
+    return result.embedding.values
+  }
+
+  // takes in an array of strings and returns embeddings
+  async batchEmbedDocuments(data: string[]) {
+    function convertData(text: string) {
+      return {
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        content: {
+          role: "user",
+          parts: [{ text }],
+        },
+      }
+    }
+
+    const result = await this.Model.batchEmbedContents({
+      requests: data.map(convertData)
+    })
+
+    return result.embeddings.values
+  }
+
+  async getRelivantChunks(qryString: string, topK = 5) {
+    const [embeddings, qryVector] = await Promise.all([
+      db.embeddings.findMany(),
+      this.embed(qryString)
+    ])
+
+    const sortedChunks = embeddings
+      .map(embedding => ({ cs: this.cosineSimilarity(qryVector, embedding.embedding), text: embedding.text }))
+      .sort((a, b) => (a.cs < b.cs ? 1 : -1))
+      .slice(0, topK);
+
+    console.log(">>> SORTED COSINE CHUNKS: ", sortedChunks)
+    return sortedChunks
+  }
+
+  private cosineSimilarity(a: number[], b: number[]) {
+    const dotProduct = a.reduce((t, e, i) => (t += e * b[i]!), 0)
+
+    const magnitudes = [a, b]
+      .map(e => Math.sqrt(e.reduce((t, f) => (t += f * f), 0)))
+      .reduce((t, f) => (t += f), 1);
+
+    return dotProduct / magnitudes;
+  }
+
+  async chunkPDF(URL: string, chunkSize = 1000, chunkOverlap = 100) {
+    const filePath = path.join("/tmp", path.basename(URL));
+
+    const { data: fileResponse } = await axios.get<Readable>(URL, {
+      responseType: "stream",
+    });
+
+    const writer = fs.createWriteStream(filePath);
+
+    fileResponse.pipe(writer);
+
+    await new Promise<void>((res, rej) => {
+      writer.on("finish", res);
+      writer.on("error", rej);
+    });
+
+    const parsedPDF = await pdf(fs.readFileSync(filePath))
+    console.log(">>> PDF: ", parsedPDF.text)
+
+    const words = parsedPDF.text.split(/\s+/) // split text into words
+    const chunks: string[] = []
+
+    for (let i = 0; i < words.length; i += chunkSize - chunkOverlap) {
+      const chunk = words.slice(i, i + chunkSize).join(' ');
+      chunks.push(chunk);
+      if (i + chunkSize >= words.length) break; // Stop when near the end
+    }
+
+    return chunks;
+
+  }
+
+}
 
 export class Gemini {
-  private AImodelName = { model: "gemini-1.5-pro" };
+  private AIModelName = { model: "gemini-1.5-pro" };
   private GenAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-  private Model = this.GenAI.getGenerativeModel(this.AImodelName);
+  private Model: GenerativeModel = this.GenAI.getGenerativeModel(this.AIModelName)
   private fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY!);
 
   // only takes text
@@ -125,6 +233,8 @@ export class Gemini {
   }
 
   set modelName(name: string) {
-    this.AImodelName = { model: name };
+    this.AIModelName = { model: name };
+    this.Model = this.GenAI.getGenerativeModel(this.AIModelName)
   }
 }
+
