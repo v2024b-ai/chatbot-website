80a4da4c25feb314222fe5514d095c3fca599569
Mina Boktor <148801385+minaboktor2628@users.noreply.github.com>
2024-11-26 13:45:19 +0100

    Update models-array.ts

--- src/data/models-array.ts
+++ src/data/models-array.ts
@@ -8,22 +8,22 @@ export const Models: Array<ModelCards> = [
     longDesc: "Use the chatbot to ask questions about specific VPC IQPs",
     link: "/tools/chatbot",
   },
-  {
-    title: "Data Analyzer",
-    shortDesc: "Use this chatbot to analyze CSV files",
-    longDesc:
-      "Upload a report and get back a complete podcast format" +
-      " conversation about the topics of the report",
-    link: "/tools/csv-analyzer",
-  },
-  {
-    title: "Podcast",
-    shortDesc: "Get a full podcast about any inputted report",
-    longDesc:
-      "Import a report file and generate an AI podcast on that" +
-      " specific report.",
-    link: "/tools/podcast",
-  },
+  // {
+  //   title: "Data Analyzer",
+  //   shortDesc: "Use this chatbot to analyze CSV files",
+  //   longDesc:
+  //     "Upload a report and get back a complete podcast format" +
+  //     " conversation about the topics of the report",
+  //   link: "/tools/csv-analyzer",
+  // },
+  // {
+  //   title: "Podcast",
+  //   shortDesc: "Get a full podcast about any inputted report",
+  //   longDesc:
+  //     "Import a report file and generate an AI podcast on that" +
+  //     " specific report.",
+  //   link: "/tools/podcast",
+  // },
   {
     title: "CSV Query Tool Chatbot",
     shortDesc: "Query our CSVs and ask questions to our chatbot",
