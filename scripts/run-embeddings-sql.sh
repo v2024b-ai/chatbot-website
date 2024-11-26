502d4455b2ca55e869a222144082e854e4021774
minaboktor2628 <mina26b8@gmail.com>
2024-11-26 14:31:53 +0100

    fix script

--- scripts/run-embeddings-sql.sh
+++ scripts/run-embeddings-sql.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export $(grep -v '^#' "../$SCRIPT_DIR/.env" | xargs)
+export $(grep -v '^#' "/$SCRIPT_DIR/../.env" | xargs)
 
 # run both sql files in the data folder to populate db
 psql $DATABASE_URL -f ../data/embeddings.sql &
